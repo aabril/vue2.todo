6cc5037923958d9fe00096bbd41b3dd4f83ab579
Albert Abril <albert.abril@gmail.com>
2017-05-04 14:16:13 +0200

    Adding vuex (redux) and store/

---
+++ src/store/store.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+
+  // Current States
+  state: {
+  },
+
+  // Showing things, not mutating state
+  getters: {
+  },
+
+  // Mutating the state ; mutations are always synchronous
+  mutations: {
+  },
+
+  // Commits the mutation, it's asynchronous
+  actions: {
+  }
+
+})
